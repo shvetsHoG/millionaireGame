4866a15c2fcca5526d3924534d11776ec5f98df5
shvetsHoG <steamk44@gmail.com>
2023-11-30 23:18:03 +0300

    change MillionaireGame

--- src/main/java/MillionaireGame.java
+++ src/main/java/MillionaireGame.java
@@ -6,11 +6,5 @@ public class MillionaireGame {
     public static void main(String[] args) {
         StartPage startPage = new StartPage();
         DatabaseMillionaireGameRequests databaseMillionaireGameRequests = new DatabaseMillionaireGameRequests();
-        databaseMillionaireGameRequests.createQuestion("Кто_президент_РФ");
-        int id = databaseMillionaireGameRequests.getQuestionId("Кто_президент_РФ");
-        databaseMillionaireGameRequests.createAnswer(id, "Путин" , true);
-        databaseMillionaireGameRequests.createAnswer(id, "Джо_Байден" , false);
-        databaseMillionaireGameRequests.createAnswer(id, "Зеленский" , false);
-        databaseMillionaireGameRequests.createAnswer(id, "Обама" , false);
     }
 }
